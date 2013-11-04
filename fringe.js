144b2df5e3307745947bb9a0ec771ef5f6b7d7ec
Lucas Riutzel <lriutzel@gmail.com>
2013-11-03 20:11:39 -0600

    add sound when player hits astroid

--- fringe.js
+++ fringe.js
@@ -476,6 +476,7 @@ var LunaticFringe = function (canvas) {
 
             // Don't die from asteroids yet. It looks cool to bounce off. Take this out when ship damage is implemented.
             if (otherObject instanceof Asteroid) {
+                mediaManager.Audio.CollisionGeneral.play();
                 log("Player hit a Asteroid");
                 this.updateHealth(-30);
                 return;
