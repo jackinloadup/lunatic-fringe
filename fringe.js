6ec600ee4eeaffbe8e541352892bf59dd3af1e84
Josh <schwal10@msu.edu>
2018-09-15 17:57:46 -0400

    Moved Vector class into its own file
    Moved the Vector helper class into its own file.

--- fringe.js
+++ fringe.js
@@ -93,23 +93,6 @@ var LunaticFringe = function (canvas) {
             delete this.keysPressed[event.keyCode];
         }
     };
-    //
-    // Vector helper class
-    var Vector = function (x, y) {
-        this.X = x || 0;
-        this.Y = y || 0;
-    }
-    Vector.prototype.Copy = function () { return new Vector(this.X, this.Y); }
-    Vector.prototype.Add = function (other) { return this.Copy()._Add(other); }
-    Vector.prototype._Add = function (other) { this.X += other.X; this.Y += other.Y; return this; }
-    Vector.prototype.Subtract = function (other) { return this.Copy()._Subtract(other); }
-    Vector.prototype._Subtract = function (other) { this.X -= other.X; this.Y -= other.Y; return this; }
-    Vector.prototype.Scale = function (scalar) { return this.Copy()._Scale(scalar); }
-    Vector.prototype._Scale = function (scalar) { this.X *= scalar; this.Y *= scalar; return this; }
-    Vector.prototype.DotProduct = function (other) { return this.X * other.X + this.Y * other.Y; }
-    Vector.prototype.SelfDotProduct = function () { return this.DotProduct(this); }
-    Vector.prototype.Normalize = function (other) { return this.Copy().Scale(1 / this.Magnitude()); }
-    Vector.prototype.Magnitude = function () { return Math.sqrt(this.SelfDotProduct()); }
 
     // This is primarily to make sure media is preloaded, otherwise projectiles only load when fire is pressed and looks funky
     this.mediaManager = new LunaticFringe.MediaManager();
