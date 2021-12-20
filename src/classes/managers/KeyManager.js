a3fb6fd4bc23d393de0cb37e3906577c3b8c5a26
schwal10 <jschwall4507@gmail.com>
2021-12-20 08:57:23 -0500

    Added debug toggle
    Added a key toggle for debug mode so that it is easy to turn debug mode on and off while testing things without having to reload the whole game.

--- src/classes/managers/KeyManager.js
+++ src/classes/managers/KeyManager.js
@@ -1,3 +1,4 @@
+import { GameConfig } from "../../config/gameConfig.js";
 import { GameServiceManager } from "./GameServiceManager.js";
 
 export class KeyStateManager {
@@ -9,6 +10,7 @@ export class KeyStateManager {
     static DOWN = 40;
 	static CAPSLOCK = 20;
     static A = 65;
+    static D = 68;
 	static V = 86;
 	static B = 66;
 	static K = 75;
@@ -18,13 +20,17 @@ export class KeyStateManager {
     }
 
     static onKeyDown(event) {
-        if (event.keyCode == this.CAPSLOCK && this.keysPressed[event.keyCode] !== true) {
+        if (event.keyCode === this.CAPSLOCK && this.keysPressed[event.keyCode] !== true) {
             // If caps locks was pressed (and is not already registered as being down before this), handle pausing/unpausing depending on the current state
             GameServiceManager.toggleGamePaused(true);
-        } else if (event.keyCode == this.A && this.keysPressed[event.keyCode] !== true) {
+        } else if (event.keyCode === this.A && this.keysPressed[event.keyCode] !== true) {
             // If A was pressed (and is not already registered as being down before this), call game manager
             // to handle advancing the game by one frame (useful for debugging)
             GameServiceManager.advanceOneFrame();
+        } else if (event.keyCode === this.D && this.keysPressed[event.keyCode] !== true) {
+            // If D was pressed (and is not already registered as being down before this), toggle debug in the game config
+            // That way it starts whatever way is defined in the config, but can be toggled with a key press.
+            GameConfig.debug = !GameConfig.debug;
         }
         
         this.keysPressed[event.keyCode] = true;
